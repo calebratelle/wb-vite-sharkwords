d7c0db3a418d538cf8696a67c4e91b16df7cee1e
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-08-14 15:37:10 -0600

    mid-class

---
+++ main.js
@@ -0,0 +1,41 @@
+import './style.css';
+import getRandomWord from './src/randomWord.js'
+import setSharkImage from './src/sharkImage.js'
+import {setupWord, isLetterInWord, revealLetterInWord} from './src/word.js'
+import setupGuesses from './src/guess';
+
+const word = getRandomWord();
+document.querySelector('#app').innerHTML = `
+  <section id="shark-img"></section>
+
+  <section id="game-status"></section>
+
+  <section id="word-container"></section>
+
+  <section id="letter-buttons"></section>
+`;
+
+const initSharkwords = () => {
+  let numWrong = 0;
+  const word = getRandomWord();
+  setSharkImage(document.querySelector('#shark-img'), numWrong);
+  setupWord(document.querySelector('#word-container'), word);
+  // for debugging:
+  console.log(`[INFO] Correct word is: ${word}`);
+  let sharkImgEl = document.querySelector('#shark-img');
+  const handleGuess = (guessEvent, letter) => {
+    const button = guessEvent.target;
+    button.setAttribute('disabled', true);
+  
+   if (isLetterInWord(letter)) {
+      revealLetterInWord(letter);
+    } else {
+      numWrong += 1;
+      setSharkImage(sharkImgEl, numWrong);
+    }
+  
+  };
+  setupGuesses(document.querySelector('#letter-buttons'), handleGuess);
+  };
+  
+initSharkwords(setSharkImage);
